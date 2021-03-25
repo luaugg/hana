46249d34e5167c9e4e3c10db0c80382631360730
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-25 22:59:42 +0000

    Add map expression and parsing.

--- hana/src/hana/define/Expr.scala
+++ hana/src/hana/define/Expr.scala
@@ -1,9 +1,12 @@
 package hana.define
 
+import scala.collection.{Map => ScalaMap}
+
 sealed trait Expr
 
 object Expr {
-  case class Str(s: String) extends Expr // string literals
-  case class Num(n: Double) extends Expr // all numbers will be represented as doubles
+  case class Str(str: String) extends Expr // string literals
+  case class Num(number: Double) extends Expr // number literals, including decimals
   case class Ident(name: String) extends Expr // things like param names
+  case class Map(map: ScalaMap[Expr, Expr]) extends Expr
 }
--- hana/src/hana/define/Parser.scala
+++ hana/src/hana/define/Parser.scala
@@ -4,9 +4,10 @@ import fastparse._
 import MultiLineWhitespace._
 
 object Parser {
-  def string[_: P] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Expr.Str)
-  def identifier[_: P] = P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0)).!.map(Expr.Ident)
-  def expr[_: P] = P(string | identifier | number) // number last as it captures all
+  def string[_: P] = string0.map(Expr.Str)
+  def identifier[_: P] = identifier0.!.map(Expr.Ident)
+  def expr[_: P] = P(string | identifier | map | number)
+  def map[_: P] = P("%{" ~/ (expr ~ "->" ~/ expr).rep(0, ",") ~ "}").map(entries => Expr.Map(entries.toMap))
 
   def number[_: P] = P(integer0.!.? ~ ".".!.? ~ integer0.!.?).map {
     case (_, Some(_), None) => throw new NumberFormatException("no fractional component in decimal literal")
@@ -16,5 +17,7 @@ object Parser {
     case _ => throw new NumberFormatException("attempted number parsing but nothing to parse?")
   }
 
+  def string0[_: P] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"")
+  def identifier0[_: P] = P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0))
   def integer0[_: P] = P(CharsWhileIn("0-9"))
 }
