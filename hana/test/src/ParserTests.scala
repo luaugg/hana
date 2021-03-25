d4897232c97ca9d0f7d2ad1ae4435cddbd5ea576
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-25 04:41:18 +0000

    Switch to explicit asserts for cleaner debug info.

--- hana/test/src/ParserTests.scala
+++ hana/test/src/ParserTests.scala
@@ -5,27 +5,46 @@ import hana.define.Expr
 
 object ParserTests extends TestSuite {
   val tests = Tests {
-    test("parse string") {
-      val Parsed.Success(Expr.Str("Hello, world!"), _) = parse("\"Hello, world!\"", string(_))
+    test("parse_string") {
+      val Parsed.Success(Expr.Str(str), _) = parse("\"Hello, world!\"", string(_))
+      str ==> "Hello, world!"
     }
 
-    test("parse numbers") {
-      test("parse integer") {
-        val Parsed.Success(Expr.Num(123), _) = parse("123", number(_))
+    test("parse_numbers") {
+      test("parse_integer") {
+        val Parsed.Success(Expr.Num(num), _) = parse("1_2_3", number(_))
+        num ==> 123
       }
 
-      test("parse big integer") {
-        val Parsed.Success(Expr.Num(Double.PositiveInfinity), _) = parse("1".repeat(400), number(_))
+      test("parse_big_integer") {
+        val Parsed.Success(Expr.Num(result), _) = parse("1".repeat(400), number(_))
+        result ==> Double.PositiveInfinity
       }
 
-      test("parse decimal") {
-        val Parsed.Success(Expr.Num(1.5), _) = parse("1.5", number(_))
-        // fails with 1.0 (stops at non-numeric)
+      test("parse_decimal") {
+        test("parse_decimal_with_two_points") {
+          parse("1.5.2", number(_)) // will fail.
+        }
+
+        test("parse_decimal_at_end") {
+          parse("1.", number(_))
+        }
+
+        test("parse_normal_decimal") {
+          val Parsed.Success(Expr.Num(num), _) = parse("1.5", number(_))
+          num ==> 1.5
+        }
+
+        test("parse_decimal_at_start") {
+          val Parsed.Success(Expr.Num(num), _) = parse(".5", number(_))
+          num ==> .5
+        }
       }
     }
 
     test("parse identifier") {
-      val Parsed.Success(Expr.Ident("_a1"), _) = parse("_a1", identifier(_))
+      val Parsed.Success(Expr.Ident(ident), _) = parse("_a1", identifier(_))
+      ident ==> "_a1"
     }
   }
 }
