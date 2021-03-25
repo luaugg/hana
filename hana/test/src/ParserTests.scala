0d260ec9c2ebaab639ddec1a72bd78fe66002de5
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-25 03:19:19 +0000

    Add string and number test cases.

--- hana/test/src/ParserTests.scala
+++ hana/test/src/ParserTests.scala
@@ -1,11 +1,27 @@
 import utest._
 import hana.define.Parser._
-import fastparse.parse
+import fastparse.{Parsed, parse}
+import hana.define.Expr
 
 object ParserTests extends TestSuite {
   val tests = Tests {
     test("parse string") {
-      parse("\"Hello, world!", string(_))
+      val Parsed.Success(Expr.Str("Hello, world!"), _) = parse("\"Hello, world!\"", string(_))
+    }
+
+    test("parse numbers") {
+      test("parse integer") {
+        val Parsed.Success(Expr.Num(123), _) = parse("123", number(_))
+      }
+
+      test("parse big integer") {
+        val Parsed.Success(Expr.Num(Double.PositiveInfinity), _) = parse("1".repeat(400), number(_))
+      }
+
+      test("parse decimal") {
+        val Parsed.Success(Expr.Num(1.5), _) = parse("1.5", number(_))
+        // fails with 1.0 (stops at non-numeric)
+      }
     }
   }
 }
