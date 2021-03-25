48eb1c1be2a5ae21493613a58bfa0c27903f35b2
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-25 04:41:33 +0000

    Update number definition.

--- hana/src/hana/define/Parser.scala
+++ hana/src/hana/define/Parser.scala
@@ -5,7 +5,7 @@ import MultiLineWhitespace._
 
 object Parser {
   def string[_: P] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Expr.Str)
-  def number[_: P] = P(CharsWhileIn("0-9")).!.map(_.toDouble).map(Expr.Num)
+  def number[_: P] = P(CharIn("0-9.") ~~ CharsWhileIn("0-9._", 0)).!.map(_.toDouble).map(Expr.Num)
   def identifier[_: P] = P(CharIn("a-zA-Z_") ~~ CharsWhileIn("a-zA-Z0-9_", 0)).!.map(Expr.Ident)
   def expr[_: P] = P(string | number | identifier)
 }
