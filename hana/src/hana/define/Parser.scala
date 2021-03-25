b3a614ed6dd0c045e2577a073db0fee16d18bde0
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-25 22:05:07 +0000

    Change expr parser order, and make tests use expr parser.

--- hana/src/hana/define/Parser.scala
+++ hana/src/hana/define/Parser.scala
@@ -6,7 +6,7 @@ import MultiLineWhitespace._
 object Parser {
   def string[_: P] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Expr.Str)
   def identifier[_: P] = P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0)).!.map(Expr.Ident)
-  def expr[_: P] = P(string | number | identifier)
+  def expr[_: P] = P(string | identifier | number) // number last as it captures all
 
   def number[_: P] = P(integer0.!.? ~ ".".!.? ~ integer0.!.?).map {
     case (_, Some(_), None) => throw new NumberFormatException("no fractional component in decimal literal")
--- hana/test/src/ParserTests.scala
+++ hana/test/src/ParserTests.scala
@@ -1,51 +1,51 @@
 import utest._
-import hana.define.Parser._
+import hana.define.Parser.expr
 import fastparse.{Parsed, parse}
 import hana.define.Expr
 
 object ParserTests extends TestSuite {
   val tests = Tests {
     test("parse_string") {
-      val Parsed.Success(Expr.Str(str), _) = parse("\"Hello, world!\"", string(_))
+      val Parsed.Success(Expr.Str(str), _) = parse("\"Hello, world!\"", expr(_))
       str ==> "Hello, world!"
     }
 
     test("parse_numbers") {
       test("parse_integer") {
-        val Parsed.Success(Expr.Num(num), _) = parse("123", number(_))
+        val Parsed.Success(Expr.Num(num), _) = parse("123", expr(_))
         num ==> 123
       }
 
       test("parse_big_integer") {
-        val Parsed.Success(Expr.Num(result), _) = parse("1".repeat(400), number(_))
+        val Parsed.Success(Expr.Num(result), _) = parse("1".repeat(400), expr(_))
         result ==> Double.PositiveInfinity
       }
 
       test("parse_decimal") {
         test("parse_decimal_with_two_points") {
           // todo: will silently ignore. add strict error messages
-          val Parsed.Success(Expr.Num(result), _) = parse("1.5.2", number(_))
+          val Parsed.Success(Expr.Num(result), _) = parse("1.5.2", expr(_))
           result
         }
 
         test("parse_decimal_at_end") {
-          parse("1.", number(_))
+          parse("1.", expr(_))
         }
 
         test("parse_normal_decimal") {
-          val Parsed.Success(Expr.Num(num), _) = parse("1.5", number(_))
+          val Parsed.Success(Expr.Num(num), _) = parse("1.5", expr(_))
           num ==> 1.5
         }
 
         test("parse_decimal_at_start") {
-          val Parsed.Success(Expr.Num(num), _) = parse(".5", number(_))
+          val Parsed.Success(Expr.Num(num), _) = parse(".5", expr(_))
           num ==> .5
         }
       }
     }
 
     test("parse identifier") {
-      val Parsed.Success(Expr.Ident(ident), _) = parse("_a1", identifier(_))
+      val Parsed.Success(Expr.Ident(ident), _) = parse("_a1", expr(_))
       ident ==> "_a1"
     }
   }
