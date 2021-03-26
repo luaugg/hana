e3f43266de1322f93a39101b7dee27f6e80ea975
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-26 03:41:55 +0000

    Remove faulty tests and include more test cases.

--- hana/test/src/ParserTests.scala
+++ hana/test/src/ParserTests.scala
@@ -1,66 +1,64 @@
-import utest._
-import hana.define.Parser.expr
 import fastparse.{Parsed, parse}
 import hana.define.Expr._
+import hana.define.Parser.{expr, list}
+import utest._
+import scala.collection.{Map => ScalaMap}
 
 object ParserTests extends TestSuite {
   val tests: Tests = Tests {
     test("parse_string") {
-      val Parsed.Success(Str(str), _) = parse("\"Hello, world!\"", expr(_))
-      str ==> "Hello, world!"
+      val Parsed.Success(Str("Hello, world!"), _) = parse("\"Hello, world!\"", expr(_))
+    }
+
+    test("parse_identifier") {
+      val Parsed.Success(Ident("_a1"), _) = parse("_a1", expr(_))
     }
 
     test("parse_numbers") {
       test("parse_integer") {
-        val Parsed.Success(Num(num), _) = parse("123", expr(_))
-        num ==> 123
+        val Parsed.Success(Num(123), _) = parse("123", expr(_))
       }
 
       test("parse_big_integer") {
-        val Parsed.Success(Num(result), _) = parse("1".repeat(400), expr(_))
-        result ==> Double.PositiveInfinity
+        val Parsed.Success(Num(Double.PositiveInfinity), _) = parse("1".repeat(400), expr(_))
       }
 
       test("parse_decimal") {
-        test("parse_decimal_with_two_points") {
-          // todo: will silently ignore. add strict error messages
-          val Parsed.Success(Num(result), _) = parse("1.5.2", expr(_))
-          result
-        }
-
-        test("parse_decimal_at_end") {
-          parse("1.", expr(_))
-        }
-
         test("parse_normal_decimal") {
-          val Parsed.Success(Num(num), _) = parse("1.5", expr(_))
-          num ==> 1.5
+          val Parsed.Success(Num(1.5), _) = parse("1.5", expr(_))
         }
 
         test("parse_decimal_at_start") {
-          val Parsed.Success(Num(num), _) = parse(".5", expr(_))
-          num ==> .5
+          val Parsed.Success(Num(.5), _) = parse(".5", expr(_))
         }
       }
     }
 
-    test("parse identifier") {
-      val Parsed.Success(Ident(ident), _) = parse("_a1", expr(_))
-      ident ==> "_a1"
+    test("parse_maps") {
+      test("parse_empty_map") {
+        val Parsed.Success(Map(map), _) = parse("{}", expr(_))
+        map ==> ScalaMap.empty
+      }
+
+      test("parse_occupied_map") {
+        val Parsed.Success(Map(map), _) = parse("{abc -> 54321, \"foo\" -> bar}", expr(_))
+        map(Str("foo")) ==> Ident("bar")
+        map(Ident("abc")) ==> Num(54321)
+      }
     }
 
-    test("parse map") {
-      val Parsed.Success(Map(map), _) = parse("{abc -> 54321, \"foo\" -> bar}", expr(_))
-      map(Str("foo")) ==> Ident("bar")
-      map(Ident("abc")) ==> Num(54321)
-    }
+    test("parse_lists") {
+      test("parse_empty_list") {
+        val Parsed.Success(List(seq), _) = parse("[]", expr(_))
+        seq ==> Seq.empty
+      }
 
-    test("parse list") {
-      val Parsed.Success(List(seq), _) = parse("[1, \"foo\", bar, {}]", expr(_))
-      seq.head ==> Num(1)
-      seq(1) ==> Str("foo")
-      seq(2) ==> Ident("bar")
-      seq.tail ==> Map(scala.collection.Map.empty)
+      test("parse_occupied_list") {
+        val Parsed.Success(List(Seq(Num(1), Str("foo"), Ident("bar"), Map(map))), _) =
+          parse("[1, \"foo\", bar, {}]", expr(_))
+
+        map ==> ScalaMap.empty
+      }
     }
   }
 }
