928d52f81c38d4b72e4ca8e286716356f2ad6169
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-26 03:03:08 +0000

    Add list parser to expr parser.

--- hana/src/hana/define/Parser.scala
+++ hana/src/hana/define/Parser.scala
@@ -7,7 +7,7 @@ import Expr._
 object Parser {
   def string[_: P]: P[Str] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Str)
   def identifier[_: P]: P[Ident] =  P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0)).!.map(Ident)
-  def expr[_: P]: P[Expr] = P(string | identifier | map | number)
+  def expr[_: P]: P[Expr] = P(string | identifier | list | map | number)
   def map[_: P]: P[Map] = P("{" ~/ (expr ~ "->" ~/ expr).rep(0, ",") ~ "}").map(entries => Map(entries.toMap))
   def list[_: P]: P[List] = P("[" ~/ expr.rep(0, ", ") ~ "]").map(List)
 
