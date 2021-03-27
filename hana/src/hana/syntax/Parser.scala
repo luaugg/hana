512bc4a7af37ae9442906d2b39cc58fd0654a0bb
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-27 09:55:15 +0000

    small refactor

--- hana/src/hana/syntax/Parser.scala
+++ hana/src/hana/syntax/Parser.scala
@@ -13,13 +13,12 @@ object Parser {
   def string[_: P]: P[Str] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Str)
   def map[_: P]: P[Map] = P(occupiedMap | emptyMap)
   def list[_: P]: P[List] = P(occupiedList | emptyList)
-  def number[_: P]: P[Num] = P((digits.? ~ "." ~ digits ~ !".") | digits).!
-    .map(str => Num(str.replace("_", "").toDouble))
+  def number[_: P]: P[Num] = P(decimal | digits).!.map(str => Num(str.replace("_", "").toDouble))
   def identifier[_: P]: P[Ident] = P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0)).!
     .filter(!keywords.contains(_))
     .map(Ident)
 
-
+  private def decimal[_: P] = P(digits.? ~ "." ~ digits ~ !".")
   private def emptyMap[_: P] = P("{}").map(_ => Map(ScalaMap.empty))
   private def emptyList[_: P] = P("[]").map(_ => List(Seq.empty))
   private def occupiedMap[_: P] = P("{" ~/ (expr ~/ "->" ~/ expr).rep(0, ",") ~ "}").map(entries => Map(entries.toMap))
