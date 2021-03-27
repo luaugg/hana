45ddd1a4b983bfbe7631c8abbb9550e80c506c49
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-27 09:29:07 +0000

    Fix TestUtils. (wrong type)

--- hana/test/src/TestUtils.scala
+++ hana/test/src/TestUtils.scala
@@ -2,7 +2,7 @@ import hana.syntax.Parser.expr
 import fastparse._
 
 object TestUtils {
-  def extract[T](code: String, parser: Any = expr(_)): T = {
+  def extract[T](code: String, parser: P[_] => P[_] = expr(_)): T = {
     val Parsed.Success(value, _) = parse(code, parser)
     value.asInstanceOf[T]
   }
