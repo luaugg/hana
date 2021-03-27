711e26e02afcd8cf3c8c6783808b841ffb533c54
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-27 08:51:19 +0000

    Switch to single-line whitespace consumer.

--- hana/src/hana/syntax/Parser.scala
+++ hana/src/hana/syntax/Parser.scala
@@ -1,12 +1,12 @@
 package hana.syntax
 
 import fastparse._
-import MultiLineWhitespace._
-import hana.define.Expr
-import hana.define.Expr._
+import SingleLineWhitespace._
+import hana.define.Literals
+import hana.define.Literals._
 import scala.collection.{Map => ScalaMap}
 
-object Literals {
+object Parser {
   private val keywords = Seq("def", "do", "end", "true", "false", "or", "not", "if", "else", "and", "use")
 
   def string[_: P]: P[Str] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Str)
@@ -16,8 +16,7 @@ object Literals {
 
   def map[_: P]: P[Map] = P(emptyMap | occupiedMap)
   def list[_: P]: P[List] = P(emptyList | occupiedList)
-  def expr[_: P]: P[Expr] = P(string | identifier | list | map | number)
-
+  def expr[_: P]: P[Literals] = P(string | identifier | list | map | number)
   def number[_: P]: P[Num] = P(CharsWhileIn("0-9").!.? ~ ".".!.? ~ CharsWhileIn("0-9").!.?).map {
     case (_, Some(_), None) => throw new NumberFormatException("no fractional component in decimal literal")
     case (Some(integer), None, None) => Num(integer.toDouble)
@@ -28,6 +27,6 @@ object Literals {
 
   private def emptyMap[_: P] = P("{}").map(_ => Map(ScalaMap.empty))
   private def emptyList[_: P] = P("[]").map(_ => List(Seq.empty))
-  private def occupiedMap[_: P] = P("{" ~/ (expr ~ "->" ~/ expr).rep(0, ",") ~ "}").map(entries => Map(entries.toMap))
-  private def occupiedList[_: P] = P("[" ~/ expr.rep(0, ", ") ~ "]").map(List)
+  private def occupiedMap[_: P] = P("{" ~/ (expr ~/ "->" ~/ expr).rep(0, ",") ~ "}").map(entries => Map(entries.toMap))
+  private def occupiedList[_: P] = P("[" ~/ expr.rep(0, ",") ~ "]").map(List)
 }
