5a135f6b7e38c86b0ba0d4c30d518e78a7c26028
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-27 08:58:18 +0000

    Update number parser to something that isn't brain-dead.

--- hana/src/hana/syntax/Parser.scala
+++ hana/src/hana/syntax/Parser.scala
@@ -9,24 +9,19 @@ import scala.collection.{Map => ScalaMap}
 object Parser {
   private val keywords = Seq("def", "do", "end", "true", "false", "or", "not", "if", "else", "and", "use")
 
+  def expr[_: P]: P[Literals] = P(string | identifier | list | map | number)
   def string[_: P]: P[Str] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Str)
+  def map[_: P]: P[Map] = P(emptyMap | occupiedMap)
+  def list[_: P]: P[List] = P(emptyList | occupiedList)
+  def number[_: P]: P[Num] = P(digits.? ~ ("." ~ digits ~ !".").?).!.map(str => Num(str.toDouble))
   def identifier[_: P]: P[Ident] = P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0)).!
     .filter(!keywords.contains(_))
     .map(Ident)
 
-  def map[_: P]: P[Map] = P(emptyMap | occupiedMap)
-  def list[_: P]: P[List] = P(emptyList | occupiedList)
-  def expr[_: P]: P[Literals] = P(string | identifier | list | map | number)
-  def number[_: P]: P[Num] = P(CharsWhileIn("0-9").!.? ~ ".".!.? ~ CharsWhileIn("0-9").!.?).map {
-    case (_, Some(_), None) => throw new NumberFormatException("no fractional component in decimal literal")
-    case (Some(integer), None, None) => Num(integer.toDouble)
-    case (Some(integer), Some(_), Some(fraction)) => Num(integer.toDouble + fraction.toDouble / 10)
-    case (_, _, Some(fraction)) => Num(fraction.toDouble / 10)
-    case _ => throw new NumberFormatException("attempted number parsing but nothing to parse?")
-  }
 
   private def emptyMap[_: P] = P("{}").map(_ => Map(ScalaMap.empty))
   private def emptyList[_: P] = P("[]").map(_ => List(Seq.empty))
   private def occupiedMap[_: P] = P("{" ~/ (expr ~/ "->" ~/ expr).rep(0, ",") ~ "}").map(entries => Map(entries.toMap))
   private def occupiedList[_: P] = P("[" ~/ expr.rep(0, ",") ~ "]").map(List)
+  private def digits[_: P] = P(CharsWhileIn("0-9"))
 }
