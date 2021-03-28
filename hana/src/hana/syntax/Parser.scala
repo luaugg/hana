835ece4709ae0e355ea60224b55a692bab85947e
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-28 08:44:12 +0100

    yet another refactor

--- hana/src/hana/syntax/Parser.scala
+++ hana/src/hana/syntax/Parser.scala
@@ -9,22 +9,29 @@ import scala.collection.{Map => ScalaMap}
 object Parser {
   private val keywords = Seq("def", "do", "end", "true", "false", "or", "not", "if", "else", "and", "use")
 
-  /* We parse every line and obtain a tree of expressions from that. */
-  def line[_: P]: P[Seq[Literals]] = P(tokenStart | comment).rep
-  def expr[_: P]: P[Literals] = P(identifier | number | string | list | map | function)
-
   def identifier[_: P]: P[Ident] = P(CharIn("a-zA-Z_") ~ CharsWhileIn("a-zA-Z0-9_", 0)).!
     .filter(!keywords.contains(_))
     .map(Ident)
 
+  // String parsing
   def string[_: P]: P[Str] = P("\"" ~~/ CharsWhile(_ != '"', 0).! ~~ "\"").map(Str)
-  def map[_: P]: P[Map] = P(occupiedMap | emptyMap)
-  def list[_: P]: P[List] = P(occupiedList | emptyList)
 
+  // Map parsing
+  def map[_: P]: P[Map] = P(occupiedMap | emptyMap)
+  private def emptyMap[_: P] = P("{}").map(_ => Map(ScalaMap.empty))
+  private def occupiedMap[_: P] = P("{" ~/ (expr ~/ "->" ~/ expr).rep(sep = ",") ~ "}").map(kv => Map(kv.toMap))
+
+  // List parsing
+  def list[_: P]: P[List] = P(occupiedList | emptyList)
+  private def emptyList[_: P] = P("[]").map(_ => List(Seq.empty))
+  private def occupiedList[_: P] = P("[" ~/ expr.rep(sep = ",") ~ "]").map(List)
+
+  // Number parsing
   def number[_: P]: P[Num] = P(decimal | digits).!.map(str => Num(str.replace("_", "").toDouble))
   private def decimal[_: P] = P(digits.? ~ "." ~ digits ~ !".")
   private def digits[_: P] = P(CharIn("0-9") ~ (CharsWhileIn("0-9_") ~ !"_").?)
 
+  // Function parsing
   def function[_: P]: P[Function] = P("def" ~~/ &(" ") ~/ identifier ~ functionArgs ~/ block).map {
     case (Ident(name), Some(args), body) => Function(name, args.map(_.name), body)
     case (Ident(name), _, body) => Function(name, Seq.empty, body)
@@ -32,10 +39,9 @@ object Parser {
   private def functionArgs[_: P] = P("(" ~/ identifier.rep(sep = ",").? ~ ")").log
   private def block[_: P] = P("do" ~/ line ~ "end")
 
-  private def emptyMap[_: P] = P("{}").map(_ => Map(ScalaMap.empty))
-  private def emptyList[_: P] = P("[]").map(_ => List(Seq.empty))
-  private def occupiedMap[_: P] = P("{" ~/ (expr ~/ "->" ~/ expr).rep(sep = ",") ~ "}").map(kv => Map(kv.toMap))
-  private def occupiedList[_: P] = P("[" ~/ expr.rep(sep = ",") ~ "]").map(List)
+  // General/whitespace/comment parsing.
+  def line[_: P]: P[Seq[Literals]] = P(tokenStart | comment).rep
+  def expr[_: P]: P[Literals] = P(identifier | number | string | list | map | function)
 
   private def comment[_: P] = P("#" ~ AnyChar.rep(0)).map(_ => Empty())
   private def tokenStart[_: P] = P((CharIn(";\n\f\r") | Start) ~ expr.?).map {
