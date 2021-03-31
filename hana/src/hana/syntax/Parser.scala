b0338a27e3297d6a855361e9b8336d0833dfc8ad
Sam Pritchard <sampritchard0801@gmail.com>
2021-03-31 13:44:14 +0100

    Add function parsing again.

--- hana/src/hana/syntax/Parser.scala
+++ hana/src/hana/syntax/Parser.scala
@@ -18,8 +18,11 @@ object Parser {
   def number[_: P]: P[Num] = P(decimal | digits).!.map(str => Num(str.replace("_", "").toDouble))
   def map[_: P]: P[Map] = P("{" ~/ (expr ~/ ("->" | "to") ~/ expr).rep(0, ",") ~ "}").map(kv => Map(kv.toMap))
   def list[_: P]: P[List] = P("[" ~/ expr.rep(0, ",") ~ "]").map(List)
+  def function[_: P]: P[Function] = P("def" ~/ S ~ identifier ~/ "(" ~/ identifier.rep(0, ", ") ~ ")" ~/ block).map {
+    case (Ident(name), args, body) => Function(name, args.map(_.name), body)
+  }
 
-  def expr[_: P]: P[Expr] = P((number | identifier | string | list | map) ~/ suffixToken.?).map {
+  def expr[_: P]: P[Expr] = P((number | identifier | string | function | list | map) ~/ suffixToken.?).map {
     case (Ident(name), Some(MatchToken(right))) => Match(name, right)
     case (Ident(name), Some(CallToken(args))) => Call(name, args)
     case (exp, _) => exp
@@ -33,11 +36,13 @@ object Parser {
 
   private def decimal[_: P] = P(digits.? ~ "." ~ digits ~ !".")
   private def digits[_: P] = P(CharIn("0-9") ~ (CharsWhileIn("0-9_") ~ !"_").?)
+  private def block[_: P]: P[Seq[Expr]] = P("do" ~/ S ~ line ~ "end")
   private def comment[_: P] = P("#" ~ AnyChar.rep(0)).map(_ => Empty())
   private def tokenStart[_: P] = P((CharIn(";\n\f\r") | Start) ~ expr.?).map {
     case Some(expr) => expr
     case _ => Empty()
   }
+  private def S[_: P] = P(&(" "))
 }
 
 sealed trait Tokens
